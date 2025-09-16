eac101004965112cd7238eae3f84c7081ee3d2b7
midde <sandeep.m@hiscopetech.com>
2025-09-16 21:44:08 +0530

    updated

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,14 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-        <div >
-          This is our Project 
-          <h2 className="hello">hiiiii</h2>
-        </div>
+      <div>
+        This is our Project and this project is about
+        <h2 className="hello">hiiiii</h2>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
